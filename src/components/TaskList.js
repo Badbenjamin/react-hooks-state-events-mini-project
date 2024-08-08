412ce3f1b2e23403641211cd514b16c0d36109e7
ben mccarthy <bmccarthy90@gmail.com>
2024-08-08 18:22:47 -0400

    first deliverable task list complete

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,16 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({TASKS}) {
+// console.log(TASKS)
+  const eachTask = TASKS.map(task => {
+    // console.log(task.text)
+    return <Task key={task.text} text={task.text} />
+    
+  })
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {eachTask}
     </div>
   );
 }
