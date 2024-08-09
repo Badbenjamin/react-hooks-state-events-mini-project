95819a0b5b9228a7bdf7c5839f47494dcf5d19e4
Ben McCarthy <bmccarthy90@gmail.com>
2024-08-08 20:21:28 -0400

    category filter button selection working

--- src/components/App.js
+++ src/components/App.js
@@ -5,27 +5,35 @@ import TaskList from "./TaskList";
 import { useState } from "react";
 
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
+// console.log("Here's the data you're working with");
+// console.log({ CATEGORIES, TASKS });
 
 function App() {
-const [myTasks, setMyTasks] = useState(TASKS)
+  const [myTasks, setMyTasks] = useState(TASKS)
+  // const [filterState, setFilterState] = useState("")
+  const [isSelected, setIsSelected] = useState("")
 
-function deleteTask (text){
-  
-  // Look over this logic
-  const newTasks = myTasks.filter(task => {
-    return task.text !== text;
-  })
-  setMyTasks(newTasks)
-}
+  function deleteTask(text) {
+
+    const newTasks = myTasks.filter(task => {
+      return task.text !== text;
+    })
+    setMyTasks(newTasks)
+  }
+
+  function handleClick(category) {
+    const selectedCategory = category
+    setIsSelected(selectedCategory)
+
+  }
+
 
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
+      <CategoryFilter isSelected={isSelected} categories={CATEGORIES} onClick={handleClick} />
       <NewTaskForm />
-      <TaskList tasks={myTasks} deleteTask={deleteTask}/>
+      <TaskList tasks={myTasks} deleteTask={deleteTask} />
     </div>
   );
 }
--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,18 @@
 import React from "react";
+// import { useState } from "react";
 
-function CategoryFilter() {
+function CategoryFilter({ categories, onClick, isSelected }) {
+
+  console.log(isSelected)
+  // console.log(categorie
+  const categoryButton = categories.map(category => {
+    return <button onClick={() => { onClick(category) }} key={category} className={(isSelected === category) ? "selected" : ""}>{category}</button>
+  })
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {categoryButton}
     </div>
   );
 }
